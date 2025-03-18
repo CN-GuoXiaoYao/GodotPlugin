7035339b5f969fc4f8e31097586918798f2587c1
GuoXiaoyao <68910339+CN-GuoXiaoYao@users.noreply.github.com>
2025-03-18 13:19:44 +0800

    Update 2DLineMap.gd

--- LineMap2D/2DLineMap.gd
+++ LineMap2D/2DLineMap.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 func _enter_tree() -> void:
 	var icon = get_editor_interface().get_editor_theme().get_icon("Line2D", "EditorIcons")
-	add_custom_type("LineMap2D", "Line2D", preload("res://addons/2DLineMap/src/LineMap2D.gd"),icon)
+	add_custom_type("LineMap2D", "Line2D", preload("res://addons/LineMap2D/src/LineMap2D.gd"),icon)
 	pass
 
 
